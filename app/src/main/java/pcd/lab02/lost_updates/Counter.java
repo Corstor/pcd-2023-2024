7b4322ff55649c0e333e235a74a6be959c6ed184
Corrado Stortini <Corrado.Stortini2@studio.unibo.it>
2024-03-04 14:47:20 +0100

    Complete lost update solve

---
+++ app/src/main/java/pcd/lab02/lost_updates/Counter.java
@@ -0,0 +1,6 @@
+package pcd.lab02.lost_updates;
+
+public interface Counter {
+    int getValue();
+    void inc();
+}
---
+++ app/src/main/java/pcd/lab02/lost_updates/SafeCounter.java
@@ -0,0 +1,19 @@
+package pcd.lab02.lost_updates;
+
+public class SafeCounter implements Counter{
+    private int cont;
+	
+	public SafeCounter(int base){
+		this.cont = base;
+	}
+	
+	public void inc(){
+        synchronized(this) {
+            this.cont++;
+        }
+	}
+	
+	public int getValue(){
+		return cont;
+	}
+}
